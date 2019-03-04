cc4732acf6047ea44de82e2325650e5756ede16e
lgblkb <dbakhtiyarov@nu.edu.kz>
2019-03-04 12:36:51 +0600

    Case 1

--- egistic_navigation/global_support.py
+++ egistic_navigation/global_support.py
@@ -4,9 +4,18 @@ import pandas as pd
 import itertools
 import collections
 from lgblkb_tools.global_support import *
-from lgblkb_tools.log_support import with_logging,create_logger
+from lgblkb_tools.log_support import with_logging,create_logger,simple_fmt_no_level
+from lgblkb_tools.databases import sqla_orms,db_management as dbm
+from box import Box
+import socket
 
-simple_logger=create_logger('logs')
+is_local_dev=os.environ.get('lgblkb') or socket.gethostname() in ['lgblkb-GT62VR-7RD']
+simple_logger=create_logger('logs',log_format=simple_fmt_no_level)
+project_folder=Folder(__file__)
+configs=Box.from_yaml(filename=project_folder.get_filepath('configs_lgblkb.yaml' if is_local_dev else 'configs.yaml')).pg
+mgr=dbm.Manager(configs.image_backend,host='94.247.135.91',username='docker',password='docker',port='8086')
+
+
 
 def main():
 	pass
---
+++ egistic_navigation/qweqwe.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+	INPUT=np.array(
+		[[1,3,-2]
+			,[2,1,-3]
+			,[3,1,1]
+			,[2,2,3]
+			,[1,2,2]
+			,[2,3,0]])
+
+
+
+if __name__=='__main__':
+	main()
